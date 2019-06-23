cabdb7d969d23485b171aaa33f20c55568c41675
omar0salem11 <45674270+omar0salem11@users.noreply.github.com>
2019-06-23 13:19:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,54 +1,49 @@
-const Discord = require('discord.js');
+onst Discord = require("discord.js");
 const client = new Discord.Client();
+const bot = new Discord.Client();
+const ms = require("ms");
+const fs = require('fs');
+const moment = require('moment');
+const request = require('request');
+const dateFormat = require('dateformat');
+const r1 = require('snekfetch');
+const jimp = require('jimp')
+const math = require('math-expression-evaluator'); 
+const child_process = require("child_process");
+const developers = ['518113780723351592'];
+const admin = "#";
+const prefix = "#";
 
 
+//=============================== - [ Bot ] - ===================================
 
-client.on('ready',  () => {
-  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'); 
-  console.log('by ');
-  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-  console.log(`Logged in as  * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
-  console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-  console.log('is online')
+
+client.on('ready', () => {
+  console.log(`Logged in as : ${client.user.tag}!`);
+  console.log(`Servers : [ " ${client.guilds.size} " ]`);
+  console.log(`Users : [ " ${client.users.size} " ]`);
+  console.log(`Channels : [ " ${client.channels.size} " ]`);
+  client.user.setActivity("offline",{type: 'WATCHING'})
+  client.user.setStatus("dnd")
 });
 
-
-
-
-
-
-const devs = ["518113780723351592"];
-const adminprefix = ["#"];
 client.on('message', message => {
-    var argresult = message.content.split(` `).slice(1).join(' ');
-      if (!devs.includes(message.author.id)) return;
-      
-  if (message.content.startsWith('p!ply')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**✅   ${argresult}**`)
-  } else 
-     if (message.content === ("leave")) {
-    message.guild.leave();        
-  } else  
-  if (message.content.startsWith('wt')) {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+if (message.content.startsWith('ply')) {
+  client.user.setGame(argresult)
+    message.channel.sendMessage(`**:white_check_mark: | ${argresult}**`).then(message => {message.delete(6000)})
+} else 
+  if (message.content.startsWith( 'wt')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`**✅   ${argresult}**`)
+      message.channel.sendMessage(`**:white_check_mark: | ${argresult}**`).then(message => {message.delete(6000)})
   } else 
   if (message.content.startsWith('ls')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`**✅   ${argresult}**`)
+      message.channel.sendMessage(`**:white_check_mark: | ${argresult}**`).then(message => {message.delete(6000)})
   } else 
-  if (message.content.startsWith('st')) {
-    client.user.setGame(argresult, "https://www.twitch.tv/idk");
-      message.channel.send(`**✅**`)
-  }
-  if (message.content.startsWith('setname')) {
-  client.user.setUsername(argresult).then
-      message.channel.send(`Changing The Name To ..**${argresult}** `)
-} else
-if (message.content.startsWith('setavatar')) {
-  client.user.setAvatar(argresult);
-    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+if (message.content.startsWith('st')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/youssef98123")
+    message.channel.sendMessage(`**:white_check_mark: | ${argresult}**`).then(message => {message.delete(6000)})
 }
 });
 
